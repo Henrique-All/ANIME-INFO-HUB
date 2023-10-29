e9176bcdbdf718a06cb956ef93c7c1d8d6aa476b
CH <carlinhos.biocraft@gmail.com>
2023-10-29 09:01:23 -0300

    Fix: Mobile touch slider and hidden btn slider

--- src/js/sliders/index.js
+++ src/js/sliders/index.js
@@ -1,8 +1,11 @@
 const slider = document.querySelectorAll(".slide");
 const btnPrev = document.getElementById("btn-prev");
 const btnNext = document.getElementById("btn-next");
+const sliderContainer = document.getElementById("slider-container");
 
 let currentSlide = 0;
+let touchStartX = 0;
+let touchEndX = 0;
 
 function hideSlider() {
   slider.forEach((item) => item.classList.remove("on"));
@@ -31,6 +34,42 @@ function prevSlider() {
   }
   showSlider();
 }
+
+function handleTouchStart(event) {
+  touchStartX = event.touches[0].clientX;
+}
+
+function handleTouchEnd(event) {
+  touchEndX = event.changedTouches[0].clientX;
+  const touchDistance = touchStartX - touchEndX;
+
+  if (touchDistance > 0) {
+    // Swipe esquerda (próximo slide)
+    nextSlider();
+  } else if (touchDistance < 0) {
+    // Swipe direita (slide anterior)
+    prevSlider();
+  }
+}
+
+sliderContainer.addEventListener("touchstart", (e) => {
+  touchStartX = e.touches[0].clientX;
+});
+
+sliderContainer.addEventListener("touchmove", (e) => {
+  e.preventDefault(); // Impede o comportamento padrão de rolagem durante o deslize.
+});
+
+sliderContainer.addEventListener("touchend", (e) => {
+  touchEndX = e.changedTouches[0].clientX;
+  const touchDistance = touchStartX - touchEndX;
+
+  if (touchDistance > 50) {
+    nextSlider(); // Swipe esquerda (próximo slide)
+  } else if (touchDistance < -50) {
+    prevSlider(); // Swipe direita (slide anterior)
+  }
+});
 
 btnNext.addEventListener("click", nextSlider);
 btnPrev.addEventListener("click", prevSlider);
